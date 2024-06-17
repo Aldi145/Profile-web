08155c93e2b59d9a8042538eff954c0386749c12
AldiMaulana7 <aldimaulana3429@gmail.com>
2024-06-18 01:34:49 +0700

    test lagi

--- src/pages/Skill/Tabs.jsx
+++ src/pages/Skill/Tabs.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 // Data structure for Tabs
 export const allTabs = [
   { icon: "🧙‍♂️", label: "Front" },
@@ -10,7 +8,7 @@ const [Front, Back] = allTabs;
 export const initialTabs = [Front, Back];
 
 // Function to get the next Tab that is not in the current list
-export function getNextTab(Tabs) {
+export default function getNextTab(Tabs) {
   const existing = new Set(Tabs);
   return allTabs.find((Tab) => !existing.has(Tab));
 }
