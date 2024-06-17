fae31976f5f168a817cc1cf44a6d63af05ae0ea1
AldiMaulana7 <aldimaulana3429@gmail.com>
2024-06-18 01:59:25 +0700

    test lagi

--- src/pages/Skill/Tabs.jsx
+++ src/pages/Skill/Tabs.jsx
@@ -8,7 +8,9 @@ const [Front, Back] = allTabs;
 export const initialTabs = [Front, Back];
 
 // Function to get the next Tab that is not in the current list
-export default function getNextTab(Tabs) {
+const getNextTab = (Tabs) => {
   const existing = new Set(Tabs);
   return allTabs.find((Tab) => !existing.has(Tab));
-}
+};
+
+export default getNextTab;
